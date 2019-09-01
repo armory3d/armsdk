86d0a2ba5801d340f80a5044f656e45909df841e
luboslenco <lubos.lenco@gmail.com>
2019-09-01 10:24:55 +0200

    Clone Kromx module

--- armory.py
+++ armory.py
@@ -305,7 +305,7 @@ def update_sdk(self,context):
     git_clone(done, sdk_path, 'armory3d/haxerecast', 'lib/haxerecast')
     git_clone(done, sdk_path, 'armory3d/zui', 'lib/zui')
     git_clone(done, sdk_path, 'armory3d/armory_tools', 'lib/armory_tools')
-    git_clone(done, sdk_path, 'armory3d/Krom_bin', 'Krom')
+    git_clone(done, sdk_path, 'armory3d/Kromx_bin', 'Krom')
     git_clone(done, sdk_path, 'armory3d/Kha', 'Kha', recursive=True)
     git_clone(done, sdk_path, 'armory3d/nodejs_bin/', 'nodejs')
 
