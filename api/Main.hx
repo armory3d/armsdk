ca94a684196235cfd6c8dfbcc2ee04e95f1c5ed2
tong <tong@disktree.net>
2020-06-07 18:47:01 +0200

    Add projectVersion field

--- api/Main.hx
+++ api/Main.hx
@@ -2,5 +2,6 @@ package ;
 class Main {
 	public static inline var projectName = 'untitled';
 	public static inline var projectPackage = 'arm';
+	public static inline var projectVersion = '1.0.0';
 	public static function main() {}
 }
